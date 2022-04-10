e829d22c78747828ef27314f29b509102176cf99
Максим Стрельников <maksim_stv@mail.ru>
2022-04-11 01:06:00 +0300

    not_bad_code_without_unit_test

--- src/main/kotlin/lab2/Main.kt
+++ src/main/kotlin/lab2/Main.kt
@@ -17,9 +17,25 @@ fun main() {
     val abc = ShapeCollector()
     abc.addShape(Rectangle(2.3,5.5, Color(144, 228, 14, 0.1), Color(144, 228, 14, 0.1)))
     abc.addShape(Square(9.0, Color(144, 228, 14, 0.1), Color(144, 228, 14, 0.1)))
-    abc.addShape(Triangle(3.0,4.0,5.0, Color(144, 228, 14, 0.1), Color(144, 228, 14, 0.1)))
+    abc.addShape(Triangle(3.0,4.0,5.0, Color(144, 228, 14, 0.1), Color(144, 229, 14, 0.1)))
+    abc.addShape(Triangle(3.0,4.0,5.0, Color(144, 230, 14, 0.1), Color(144, 229, 14, 0.1)))
+    abc.addShape(Triangle(3.0,4.0,5.0, Color(144, 222, 14, 0.1), Color(144, 229, 14, 0.1)))
 
-    println(abc.getShapeMinSquare())
-    println(abc.getShapeMaxSquare())
+
+    println(abc.getShapeMinSquare()?.calcArea())
+    println(abc.getShapeMaxSquare()?.calcArea())
+    println("Shape min square is ${abc.getShapeMinSquare().toString()}, it square is ${abc.getShapeMinSquare()?.calcArea()}")
+    println(abc.sumAllSquare())
+    println(abc.seekShapesColorBorder(Color(144, 228, 14, 0.1)))
+    println(abc.seekShapesColorFill(Color(144, 228, 14, 0.1)))
+    println(abc.getAmountShapes())
+    println(abc.getShapes())
+    println(abc.getGroupColorBorder())
+    println(abc.getGroupColorFill())
+
+    println(abc.getCertainFigure("Circle"))
+    println(abc.getCertainShape(Circle::class.java))
+    println(abc.getCertainShape(Triangle::class.java))
+    println(abc.getCertainFigure("Triangle"))
 
 }
--- src/main/kotlin/lab2/ShapeCollector.kt
+++ src/main/kotlin/lab2/ShapeCollector.kt
@@ -14,19 +14,48 @@ class ShapeCollector(private val shapes: MutableList<ColoredShape2d> = mutableLi
         return shapes.maxByOrNull { it.calcArea() }
     }
 
-    fun sumAllSquare(){}
+    fun sumAllSquare(): Double {
+        var sumFigure = 0.0
+        shapes.forEach { it ->
+            sumFigure += it.calcArea()
+        }
+        return sumFigure
+    }
 
-    fun seekShapesColorBorder(){}
+    fun seekShapesColorBorder(color: Color): List<ColoredShape2d> {
+        return shapes.filter { it.borderColor == color }
+    }
 
-    fun seekShapesColorFill(){}
+    fun seekShapesColorFill(color: Color): List<ColoredShape2d>{
+        return shapes.filter { it.fillColor == color }
+    }
 
-    fun getShapes(){}
+    fun getShapes(): List<ColoredShape2d>{
+        return shapes
+    }
 
-    fun getAmountShapes(){}
+    fun getAmountShapes(): Int {
+        return shapes.size
+    }
 
-    fun getGroupColorBorder(){}
+    fun getGroupColorBorder(): Map<Color, List<ColoredShape2d>>{
+        return shapes.groupBy { it.borderColor }
+    }
 
-    fun getGroupColorFill(){}
+    fun getGroupColorFill(): Map<Color, List<ColoredShape2d>> {
+        return shapes.groupBy { it.fillColor }
+    }
 
+    fun<T> getCertainShape(certain: Class<T>): List<ColoredShape2d>{
+        return shapes.filter { it.javaClass == certain }
+    }
+
+    fun getCertainFigure(certain: String): List<ColoredShape2d>{
+        return shapes.filter { it.toString() == certain }
+    }
 
 }
+
+/*fun Shape2d?.printArea() {
+    return println(this?.calcArea())
+}*/
